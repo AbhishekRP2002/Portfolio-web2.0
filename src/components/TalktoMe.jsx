d9007ac314d0aa7a3fe900904b5c6d0839a6bfcc
Abhishek Ranjan <aviranjan444@gmail.com>
2023-06-10 21:53:56 +0530

    update website

---
+++ src/components/TalktoMe.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import "../Home.css"
+
+function TalktoMe(){
+    return(
+        <div>
+            <h1>Talk to me Page</h1>
+        </div>
+    );
+}
+
+export default TalktoMe;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './Home.jsx'
 import './index.css'
 import setupLocatorUI from "@locator/runtime";
+import { BrowserRouter } from 'react-router-dom/dist/index.js';
 
 if (  import.meta.env.NODE_ENV === "development") {
   setupLocatorUI();
 }
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
